fe426bd036600cbb5661b096d427b4a1c70f19db
Ndeyendour <ndeyefatou023@gmail.com>
2025-04-14 13:27:59 +0000

    salut

--- backend/server.js
+++ backend/server.js
@@ -27,9 +27,12 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
+
 app.use(cors({
-  origin: 'https://outam.netlify.app',  // Remplace par l'URL de ton frontend Netlify
+  origin: ['https://outam.netlify.app', 'http://localhost:3000'],
+  credentials: true
 }));
+
 app.use(cookieParser());
 app.use('/assets', express.static('assets'));
 app.use(express.static(path.join(__filename,"frontend/build")))
